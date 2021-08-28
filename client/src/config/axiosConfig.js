8d52c9677b7abfdc5afbcd0661091a493776c823
Akshay R R <akshayar1027@gmail.com>
2021-08-29 02:16:39 +0530

    Initial setup

---
+++ client/src/config/axiosConfig.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+// const BASE_URL = '';
+const BASE_URL = 'http://localhost:5001/';
+
+const api = axios.create({
+    baseURL: BASE_URL,
+    timeout: 20000
+});
+
+export default api;
